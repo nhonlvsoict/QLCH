b96726941f458a14c35bce3753d90bdf3f83705f
NhonLV <44114705+nhonlvsoict@users.noreply.github.com>
2025-10-05 21:40:54 +0100

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+  }
+  plugins {
+    id("com.android.application") version "8.6.1"
+    id("org.jetbrains.kotlin.android") version "2.0.20"
+    id("kotlin-kapt") version "2.0.20"
+    id("com.google.dagger.hilt.android") version "2.52"
+  }
+}
+
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories { google(); mavenCentral() }
+}
+
+rootProject.name = "LeSonPOS"
+include(":app")
