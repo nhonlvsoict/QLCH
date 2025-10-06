6b5490713c0917d9dcf4a73372b6f3e24ac22fb7
NhonLV <44114705+nhonlvsoict@users.noreply.github.com>
2025-10-06 17:08:14 +0100

    Update Kotlin plugin syntax in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,8 @@ pluginManagement {
   }
   plugins {
     id("com.android.application") version "8.6.1"
-    id("org.jetbrains.kotlin.android") version "2.0.20"
-    id("kotlin-kapt") version "2.0.20"
+    kotlin("android") version "2.0.20"
+    kotlin("kapt") version "2.0.20"
     id("com.google.dagger.hilt.android") version "2.52"
   }
 }
