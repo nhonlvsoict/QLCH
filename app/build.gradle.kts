919086ef91d026f9a64aa1b586b6f98b0e321003
NhonLV <44114705+nhonlvsoict@users.noreply.github.com>
2025-10-07 14:30:59 -0700

    Update Compose BOM and dependencies for Kotlin 2.0.10
    Updated Compose BOM version to 2024.06.00.Use Compose BOM version 2024.06.00 (Compose 1.6.x) and align lifecycle, Hilt and JavaPoet dependencies to fix the GitHub Action build failure.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,9 @@ kotlin {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))                        
     implementation("androidx.activity:activity-compose:1.9.2")
-    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:
     // Use material3 without specifying a version so that it aligns with the Compose BOM
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
