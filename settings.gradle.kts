fc933d99c56cd0e69e15949f1d7b0c28664da548
NhonLV <44114705+nhonlvsoict@users.noreply.github.com>
2025-10-07 15:09:48 -0700

    Update Kotlin and Hilt plugin versionsUpgrade Hilt plugin to 2.57.1
    Update version of com.google.dagger.hilt.android plugin from 2.54 to 2.57.1 in settings.gradle.kts to align with Hilt dependencies and JavaPoet upgrade.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
     id("com.android.application") version "8.6.1"
     kotlin("android") version "2.0.10"   // <- change from 2.0.20
     kotlin("kapt") version "2.0.10"      // <- change from 2.0.20
-    id("com.google.dagger.hilt.android") version "2.54"
+    id("com.google.dagger.hilt.android") version "2.57.1"
   }
 }
 
