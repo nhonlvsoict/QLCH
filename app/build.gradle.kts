aa4b83e208e2019bdcfbdc0a146e9c592c8a7464
NhonLV <44114705+nhonlvsoict@users.noreply.github.com>
2025-10-07 20:52:54 +0100

    bump the Hilt plugin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,8 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3:1.3.0")
+    // Use material3 without specifying a version so that it aligns with the Compose BOM
+    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.navigation:navigation-compose:2.8.2")
